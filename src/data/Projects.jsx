1ad5decc04f400402b2d8b9c332a0feb3d70613d
Sravani Vundavalli <sravani.vundavalli.se@gmail.com>
2025-07-08 14:15:51 +0200

    Add mini projects.

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,24 +1,21 @@
 export const Projects = [
-  
   {
-    thumbnail: "./images/Booking_App.png",
-    title: "Booking App",
+    thumbnail: "./images/Tic_tac_toe.png",
+    title: "Tic-Tac-Toe",
     tags: ["HTML, CSS, Javascript, Reactjs"],
-    paragraph:
-      "Your Ultimate Travel Companion: Book Hotels and Flights with Ease! your one-stop solution for all your travel needs! Whether you're planning a weekend getaway, a business trip, or a dream vacation, we've got you covered with our seamless booking experience for hotels and flights.",
-    codelink: "https://github.com/sravanivundavalli/Bookings-App",
-    projectlink: "https://Bookings.vundavalli.me/",
+    paragraph: "A mini React.js project, Tic-Tac-Toe, is a web application that lets users enjoy the classic game either with another player or against the computer. The app features a clean, user-friendly interface where players can easily make their moves. It clearly displays the current player, updates the game status in real time, and provides instant feedback on wins, losses, or draws.",
+    codelink: "https://github.com/sravanivundavalli/react-tic-tac-toe",
+    projectlink: "https://tic-tac-toe.vundavalli.me/",
     projectlinkText: "View Project",
   },
-  
+
   {
-    thumbnail: "./images/Weather_App.png",
-    title: "Weather App",
+    thumbnail: "./images/Investment_calculator.png",
+    title: "Investment Calculator",
     tags: ["HTML, CSS, Javascript, Reactjs"],
-    paragraph:
-      "Weather App is an user-friendly weather forecasting application designed to provide users with accurate and up-to-date weather information for upto 1 week for their location and beyond. Developed using HTML, CSS, JavaScript, and ReactJS, Weather App leverages the power of the OpenWeather API to deliver real-time weather data seamlessly.",
-    codelink: "https://github.com/sravanivundavalli/Weather-App",
-    projectlink: "https://weather.vundavalli.me/",
+    paragraph: "The Investment Calculator mini project is a web application designed to help users estimate the future value of their investments. Users can input their initial investment amount, annual interest rate, and investment duration through a user-friendly interface. The app then calculates and displays the projected future value, making it easy for users to plan and visualize their financial growth.",
+    codelink: "https://github.com/sravanivundavalli/Investment-Calculator",
+    projectlink: "https://investment-calculator.vundavalli.me/",
     projectlinkText: "View Project",
   },
 
@@ -45,4 +42,14 @@ export const Projects = [
     projectlinkText: "View Project",
   },
 
+  {
+    thumbnail: "./images/Booking_App.png",
+    title: "Booking App",
+    tags: ["HTML, CSS, Javascript, Reactjs"],
+    paragraph:
+      "Your Ultimate Travel Companion: Book Hotels and Flights with Ease! your one-stop solution for all your travel needs! Whether you're planning a weekend getaway, a business trip, or a dream vacation, we've got you covered with our seamless booking experience for hotels and flights.",
+    codelink: "https://github.com/sravanivundavalli/Bookings-App",
+    projectlink: "https://Bookings.vundavalli.me/",
+    projectlinkText: "View Project",
+  },
 ];
