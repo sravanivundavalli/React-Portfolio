8e3925542e636a449bb3717278aa6fffa2cd647e
Sravani Vundavalli <sravani.vundavalli.se@gmail.com>
2025-07-08 13:09:17 +0200

    Update resume.

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -42,7 +42,7 @@ export const AboutSection = () => {
             enjoy collaborating with teams to bring ideas to life.
           </p>
           <CTAButton
-            href="./resume/Sravani-Vundavalli-CV.pdf"
+            href="./resume/Sravani_Vundavalli_CV.pdf"
             download="Sravani-Vundavalli-CV.pdf"
           >
             Download Resume
